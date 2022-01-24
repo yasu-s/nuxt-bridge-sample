9266cc0240e513135fb5c342ff9af36b73236387
Yasuyuki Saito <ya_saito@uluru.jp>
2022-01-24 11:39:09 +0900

    ディレクトリ移動

--- test/components/NuxtLogo.spec.vue
+++ test/components/NuxtLogo.spec.vue
@@ -2,7 +2,7 @@
 import { mount } from '@vue/test-utils'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
-describe('NuxtLogo', () => {
+describe('@/components/NuxtLogo.vue', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(NuxtLogo)
     expect(wrapper.vm).toBeTruthy()
