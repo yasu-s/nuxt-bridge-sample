b24dd9fbedf34d8c8fb8de1d6e35846cb63e1678
Yasuyuki Saito <ya_saito@uluru.jp>
2022-01-25 19:27:51 +0900

    validate追加

---
+++ pages/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ id }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'ValidatePage',
+  validate({ params }) {
+    return /^([a-zA-Z0-9]{5})$/.test(params.id)
+  },
+  setup() {
+    return {}
+  },
+  asyncData({ params }) {
+    return { id: params.id }
+  },
+})
+</script>
---
+++ test/pages/_id/index.spec.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { shallowMount } from '@vue/test-utils'
+import ValidatePage from '@/pages/_id/index.vue'
+
+describe('@/pages/_id/index.vue', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallowMount(ValidatePage)
+    expect(wrapper.vm).toBeTruthy()
+  })
+
+  test('run validate', async () => {
+    const wrapper = shallowMount(ValidatePage)
+
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const validate = wrapper.vm.$options.validate
+    if (!validate) throw new Error('err')
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const result = validate({ params: { id: '11111' } } as any)
+    const actual = result instanceof Promise ? await result : result
+    expect(actual).toBeTruthy()
+  })
+})
+</script>
